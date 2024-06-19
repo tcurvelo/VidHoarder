897c6747423350b8b44354c3b1ca2d4b91c73622
Thiago Curvelo <tcurvelo@gmail.com>
2024-06-18 21:32:23 -0300

    simplify main script

--- bot.py
+++ bot.py
@@ -2,44 +2,52 @@ import logging
 import pathlib
 
 from decouple import config
-from telegram.ext import CommandHandler, MessageHandler, filters
 from telegram import MessageEntity, Update
-from telegram.ext import ApplicationBuilder, CommandHandler
-
-from download import download
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+from yt_dlp import YoutubeDL
 
 logger = logging.getLogger(__name__)
 
 
-async def download_video(update: Update, context):
+def download(url: str):
+    data = {}
+
+    def hook(p):
+        data["filename"] = f'{p["info_dict"]["id"]}.{p["info_dict"]["ext"]}'
+
+    params = {"postprocessor_hooks": [hook], "outtmpl": "%(id)s.%(ext)s"}
+    with YoutubeDL(params=params) as ydl:
+        data["retcode"] = ydl.download([url])
+
+    return data
+
+
+async def handler(update: Update, context):
     urls = context.args or update.message.text.split()
     logger.info(urls)
     for url in urls:
         data = download(url)
+        logger.debug(f"{data=}")
+
         with open(data["filename"], "rb") as video:
             await update.message.reply_video(video, quote=True)
 
+        logger.info(f"Removing {data['filename']}...")
         pathlib.Path(data["filename"]).unlink()
 
 
 def main(token, debug=False, port=80, webhook_url=""):
-    application = ApplicationBuilder().token(token).build()
+    app = ApplicationBuilder().token(token).build()
 
-    application.add_handler(CommandHandler("download", download_video))
-    application.add_handler(MessageHandler(filters.Entity(MessageEntity.URL), download_video))
+    app.add_handler(CommandHandler("download", handler))
+    app.add_handler(MessageHandler(filters.Entity(MessageEntity.URL), handler))
 
     if debug or not webhook_url:
-        application.run_polling()
+        app.run_polling()
     else:
         url = "/".join([webhook_url.strip("/"), token])
-        print(f"{url=}")
-
-        application.start_webhook(
-            listen="0.0.0.0",
-            port=port,
-            url_path=token,
-            webhook_url=url
-        )
+        logger.debug(f"{url=}")
+        app.start_webhook(listen="0.0.0.0", port=port, url_path=token, webhook_url=url)
 
 
 if __name__ == "__main__":
